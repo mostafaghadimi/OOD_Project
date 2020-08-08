32a0b96491096c3b53aaabeaedc6c2d5fe5b5a2a
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-08-08 16:38:11 +0430

    [back] Administrator deleted

--- back/transportation/apps/users/admin.py
+++ back/transportation/apps/users/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.apps import AppConfig
-from apps.users.models import Driver, Customer, Authorizer, Administrator, Usermodel
+from apps.users.models import Driver, Customer, Authorizer, Usermodel
 
 models = [
     Driver,
     Customer,
     Usermodel,
     Authorizer,
-    Administrator,
 ]
 
 admin.site.register(models)
